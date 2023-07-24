1321f5097ba6b8bb3e87b8412632d3963a985412
jockerasyn <a.steinhoff321@gmail.com>
2023-07-24 11:45:02 +0200

    reading json

--- test.cpp
+++ test.cpp
@@ -6,43 +6,11 @@
 #include "include/rapidjson/writer.h"
 #include "include/rapidjson/stringbuffer.h"
 #include "include/rapidjson/filereadstream.h"
+#include "include/rapidjson/writer.h"
 #include <cstdio>
 
 int main()
 {
-    MyDigit a(4), b(3.3);
-    MyDigit resultAdd = a + b;
-    std::cout << resultAdd;
-    MyDigit resultSub = a - b;
-    std::cout << resultSub;
-    MyDigit resultMul = a * b;
-    std::cout << resultMul;
-    MyDigit resultDiv = a / b;
-    std::cout << resultDiv;
-    MyDigit resultPow = a ^ b;
-    std::cout << resultPow;
-    ++a;
-    std::cout << a;
-    --b;
-    std::cout << b;
-    a += b;
-    std::cout << a;
-    a.setVal(5);
-    a -= b;
-    std::cout << a;
-    a.setVal(5);
-
-    a *= b;
-    std::cout << a;
-    a.setVal(5);
-    a /= b;
-    std::cout << a;
-    a.setVal(5);
-    int c = 10;
-    std::cout << c + a;
-    MyDigit d;
-    std::cout << d;
-    std::cout << a;
 
     Animal anim1("cow1", 120.4);
     Animal anim2;
@@ -55,10 +23,54 @@ int main()
         std::cout << *i;
 
     // using namespace rapidjson; // namespace for next operations
-    FILE *file = fopen("example1.json", "rb");
+    FILE *file = fopen("json_files/example1.json", "rb");
     char buff[65536];
     rapidjson::FileReadStream rfile(file, buff, sizeof(buff));
     rapidjson::Document doc;
     doc.ParseStream(rfile);
     fclose(file);
+
+    // checking if file read correctly
+    rapidjson::StringBuffer buffer;
+    rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
+    doc.Accept(writer);
+    std::cout << buffer.GetString() << std::endl;
 }
+
+// int main()
+// {
+//     MyDigit a(4), b(3.3);
+//     MyDigit resultAdd = a + b;
+//     std::cout << resultAdd;
+//     MyDigit resultSub = a - b;
+//     std::cout << resultSub;
+//     MyDigit resultMul = a * b;
+//     std::cout << resultMul;
+//     MyDigit resultDiv = a / b;
+//     std::cout << resultDiv;
+//     MyDigit resultPow = a ^ b;
+//     std::cout << resultPow;
+//     ++a;
+//     std::cout << a;
+//     --b;
+//     std::cout << b;
+//     a += b;
+//     std::cout << a;
+//     a.setVal(5);
+//     a -= b;
+//     std::cout << a;
+//     a.setVal(5);
+
+//     a *= b;
+//     std::cout << a;
+//     a.setVal(5);
+//     a /= b;
+//     std::cout << a;
+//     a.setVal(5);
+//     int c = 10;
+//     std::cout << c + a;
+//     MyDigit d;
+//     std::cout << d;
+//     std::cout << a;
+//     return 0;
+// }
