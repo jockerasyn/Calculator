3d065b5869eddd6cbdc8eca19b00eb3abb2ecaab
jockerasyn <a.steinhoff321@gmail.com>
2023-07-25 15:13:37 +0200

    const

--- Animal/animal.h
+++ Animal/animal.h
@@ -19,8 +19,8 @@ public:
     Animal();
     Animal(std::string name, double weight);
     ~Animal();
-    int setName(std::string name);
-    int setWeight(int weight);
+    int setName(const std::string &name);
+    int setWeight(const int &weight);
     std::string getName() const;
     double getWeight() const;
     friend std::ostream &operator<<(std::ostream &print, const Animal &obj);
